688c252d1f34c82a343a24f807da45be34591876
Amaan Uddin <mohdamaanuddin100@gmail.com>
2022-10-09 21:00:09 +0300

    fourth commit

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ document.body.style.zoom = .9 // this line of code here sets the default zoom of
 
 const header = document.querySelector('header')
 const links = document.querySelectorAll('.nav-link')
-const landingTop = document.querySelector('.landing-page') // landing-page top value is 140px and bottom is 825px
+const landing = document.querySelector('.landing-page') // landing-page top value is 140px and bottom is 825px
+// height of the landing page is 685px
+
+console.log(window.innerHeight) // the innerHeight function is a property of our window element and we do not need to specify the window object to use the .innerHeight function
+
 
 function  stickyNavbar(){
     header.classList.toggle('scrolled',window.pageYOffset > 0)
@@ -10,29 +14,11 @@ function  stickyNavbar(){
 
 function chngColor(){
     links.forEach((link) => {
-        link.classList.toggle('change',landingTop.getBoundingClientRect().top < 60 && landingTop.getBoundingClientRect().top > -625)
+        link.classList.toggle('change',landing.getBoundingClientRect().top < 55 && landing.getBoundingClientRect().top > -625)
     })
 }
 
-// function changeColor(){
-    //     links.forEach((element) => {
-        //         element.style.color = '#fff'
-        //     })
-// }
-
 window.addEventListener('scroll',() => {
     stickyNavbar()
-    chngColor() // changing the color by activating a CSS file
-    // changeColor()
+    chngColor() // changing the color by activating a CSS class
 })
-
-
-
-
-// use for logging 
-// console.log(header.getBoundingClientRect().bottom)
-// console.log(landingTop.getBoundingClientRect().top)
-function disp(){
-    console.log(landingTop.getBoundingClientRect().top)
-}
-window.addEventListener('scroll',disp)
